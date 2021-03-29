10a50d9b7d544d50b84e2670af9a752692bcf63a
Martin M <martin7mondaca@gmail.com>
2021-03-28 23:18:11 -0700

    created connection to connect to mysql database

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,25 @@
 //code to connect node to mysql
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
 
 //export connection
