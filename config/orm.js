72a6645de3e5dd0872241b1fcf11b9a53b59d5d6
Martin M <martin7mondaca@gmail.com>
2021-03-28 23:32:07 -0700

    required connection.js, started functions for selecting all, inserting one, and updating one, exported orm

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,19 @@
 //require connection.js
-
+var connection = require("./connection.js")
 // In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
 // * `selectAll()`
 // * `insertOne()`
 // * `updateOne()`
 
+var orm = {
+    selectAll: function () { },
+
+    insertOne: function () { },
+
+    updateOne: function () { },
+}
+
+
 // * Export the ORM object in `module.exports`.
+module.exports = orm;
