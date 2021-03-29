3e24ce1281a1dc0891f5161ef1234fd905847592
Martin M <martin7mondaca@gmail.com>
2021-03-28 23:40:39 -0700

    required express and burger model, started routes for getting all burgers from db, for creating a burger, and for updated a burger. exporting routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,23 @@
 //import Express, burger.js
+var express = require("express");
+
+var router = express.Router();
+
+//importing burger model
+var burger = require("../models/burger.js")
+
+router.get("/", function (req, res) {
+    burger.selectAll
+});
+
+router.post("/api/burger", function (req, res) {
+    burger.insertOne
+});
+
+router.put("api.burger/:id", function (req, res) {
+    burger.updateOne
+});
+
 
 //create router and export router
+module.exports = router;
