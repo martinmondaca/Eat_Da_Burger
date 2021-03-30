921d832606bf3f9899baa6157a40a653d0b6d292
Martin M <martin7mondaca@gmail.com>
2021-03-30 00:01:54 -0700

    updated model with all, create, update, and delete

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,28 @@
 //import orm.js
 var orm = require("../config/orm.js")
-//export burger.js
 
+var burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
+}
+
+//export burger.js
 module.exports = burger
